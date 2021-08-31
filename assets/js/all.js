99bb7b62ae7c2f3a00dadac5033a4d7be895500e
JM <s9923024@gmail.com>
2021-08-31 22:00:06 +0800

    Update 2021-08-31T14:00:00.247Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,2 +1,25 @@
 "use strict";
+
+//聽聽他們怎麼說
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
+}); //日曆
+
+var elem = document.querySelector('input[name="datepicker"]');
+var datepicker = new Datepicker(elem, {
+  nextArrow: '>',
+  prevArrow: '<',
+  buttonClass: 'btn primary'
+});
 //# sourceMappingURL=all.js.map
